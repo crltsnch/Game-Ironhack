c1705b8cd7045599cbe2af3c826133cd3345cbd4
crltsnch <crltsnch@myuax.com>
2023-11-16 23:17:48 +0100

    constantes dle juego

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const PalabraMostrada = document.querySelector(".visualizacion-palabra");
+const textoIntentos = document.querySelector(".texto-intentos b");
+const tecladoDiv = document.querySelector(".teclado");
+const imgAhorcado = document.querySelector(".caja-ahorcado img");
+const juegoModal = document.querySelector(".juego-modal");
+const jugarDeNuevo = juegoModal.querySelector("button");
+
+//Inicializando variables de juego
