216d3a0aaf98c3e4a9eda743a844f884769e2ba8
crltsnch <crltsnch@myuax.com>
2023-11-17 15:03:52 +0100

    reiniciar

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,17 @@ const juegoModal = document.querySelector(".juego-modal");
 const jugarDeNuevo = juegoModal.querySelector("button");
 
 //Inicializando variables de juego
+let palabraActual, letrasCorrectas, contadorIntentosMal;
+const intentosMaximos = 6;
+
+const reiniciarJuego = () => {
+    // Riniciando variables del juego y elementos de la interfaz de usuario
+    letrasCorrectas = [];
+    contadorIntentosMal = 0;
+    imagenAhorcado.src = "images/hangman-0.svg";
+    textoIntentos.innerText = '${contadorIntentosMal} / ${intentosMaximos}';
+    cajaPalabra.innerHTML = palabraActual.split("").map(() => '<li class="letter"></li>').join("");
+    cajaTeclado.querySelectorAll("button").forEach(btn => (btn.disabled = false));
+    modalJuego.classList.remove("show");
+}
+
