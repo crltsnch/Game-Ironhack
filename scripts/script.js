7c260c0b546a63558c73225f1e5a6b9316d55f48
crltsnch <crltsnch@myuax.com>
2023-11-17 20:01:04 +0100

    camios

--- scripts/script.js
+++ scripts/script.js
@@ -36,3 +36,35 @@ const finDelJuego = (esVictoria) => {
     modalJuego.querySelector("p").innerHTML = '${textoModal} <b>${palabraActual}</b>';
     modalJuego.classList.add("show");
 }
+
+const iniciarJuego = (button, letraClickeada) => {
+    //Comprobando si la letra clickeada está en la palabraActual
+    if (palabraActual.includes(letraClickeada)) {
+        //Mostrando todas las letras correctas en la visualizacion de la palabra
+        [...palabraActual].forEach((letra, index) => {
+            if (letra === letraClickeada) {
+                cajaPalabra.querySelectorAll("li")[index].innerText = letra;
+                letrasCorrectas.push(letra);
+                cajaPalabra.querySelectorAll("li")[index].classList.add("correcta");
+            }
+        } else {
+            //Si la letra no está en la palabraActual
+            contadorIntentosMal++;
+            textoIntentos.innerText = '${contadorIntentosMal} / ${intentosMaximos}';
+            imgAhorcado.src = 'images/hangman-${contadorIntentosMal}.svg';
+        }
+        button.disabled = true; //Desactivando el botón clickeado
+        textoIntentos.innerText = '${contadorIntentosMal} / ${intentosMaximos}';
+        //Llamando a la función finDelJuego si el usuario gana o pierde
+        if (contadorIntentosMal === intentosMaximos) return finDelJuego(false);
+        if (letrasCorrectas.length === palabraActual.length) return finDelJuego(true);
+}
+
+// creando botones dle teclado y añadiendo even listeners
+for (let i=97; i<=122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    cajaTeclado.appendChild(button);
+    button.addEventListener("click", (e) => iniciarJuego(e.target, String.fromCharCode(i)));
+    
+}
