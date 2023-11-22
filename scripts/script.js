a2bad073d0e9362d048e90b22d6052c56172f694
crltsnch <crltsnch@myuax.com>
2023-11-22 10:46:29 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const textoIntentos = document.querySelector(".texto-intentos b");
 const tecladoDiv = document.querySelector(".teclado");
 const imgAhorcado = document.querySelector(".caja-ahorcado img");
 const juegoModal = document.querySelector(".modal-juego");
-const jugarDeNuevo = juegoModal.querySelector("button");
+const jugarDeNuevo = juegoModal.querySelector(".reiniciar");
 
 //Inicializando variables de juego
 let palabraActual, letrasCorrectas, contadorIntentosMal;
@@ -22,20 +22,12 @@ const reiniciarJuego = () => {
 
 const obtenerPalabraAleatoria = () => {
     //Seleccionando una palabra aleatoria y pista del array de palabras
-    const { word, hint} = worldList[Math.floor(Math.random() * worldList.length)];
+    const {word, hint} = worldList[Math.floor(Math.random() * worldList.length)];
     palabraActual = word; //Haciendo que la palabra actual sea la palabra seleccionada
     document.querySelector(".texto-intentos b").innerText = hint; //Mostrando la pista
     reiniciarJuego();
 }
 
-const finDelJuego = (esVictoria) => {
-    //Despues de completar el juego
-    const textoModal = esVictoria ? 'Has encontrado la palabra:' : 'La palabra correcta era:';
-    modalJuego.querySelector("img").src = `images/${esVictoria ? "win" : "lose"}.gif`;
-    modalJuego.querySelector("h4").innerText = esVictoria ? '¡Felicidades!' : '¡Fin del juego!';
-    modalJuego.querySelector("p").innerHTML = `${textoModal} <b>${palabraActual}</b>`;
-    modalJuego.classList.add("show");
-}
 
 const iniciarJuego = (button, letraClickeada) => {
     //Comprobando si la letra clickeada está en la palabraActual
@@ -45,22 +37,37 @@ const iniciarJuego = (button, letraClickeada) => {
             if (letra === letraClickeada) {
                 letrasCorrectas.push(letra);
                 PalabraMostrada.querySelectorAll("li")[index].innerText = letra;
-                PalabraMostrada.querySelectorAll("li")[index].classList.add("correcta");
+                PalabraMostrada.querySelectorAll("li")[index].classList.add("adivinada");
             }
         });
                 
-        }   else {
-            //Si la letra no está en la palabraActual
-            contadorIntentosMal++;
-            imgAhorcado.src = `images/hangman-${contadorIntentosMal}.svg`;
-            }
+    } else {
+        // Si la letra no está en la palabraActual
+        contadorIntentosMal++;
+        imgAhorcado.src = `images/hangman-${contadorIntentosMal}.svg`;
+      }
 
         button.disabled = true; //Desactivando el botón clickeado
         textoIntentos.innerText = `${contadorIntentosMal} / ${intentosMaximos}`;
         //Llamando a la función finDelJuego si el usuario gana o pierde
-        if (contadorIntentosMal === intentosMaximos) return finDelJuego(false);
-        if (letrasCorrectas.length === palabraActual.length) return finDelJuego(true);
-}
+        if (contadorIntentosMal === intentosMaximos) {
+            finDelJuego(false);
+        }
+        if (letrasCorrectas.length === palabraActual.length) {
+            finDelJuego(true);
+        }
+};
+
+const finDelJuego = (esVictoria) => {
+    // Después de completar el juego
+    const textoModal = esVictoria
+      ? "Has encontrado la palabra:"
+      : "La palabra correcta era:";
+    juegoModal.querySelector("img").src = `images/${esVictoria ? "winner" : "loser"}.gif`;
+    juegoModal.querySelector("h4").innerText = esVictoria ? "¡Felicidades!" : "¡Fin del juego!";
+    juegoModal.querySelector("p").innerHTML = `${textoModal} <b>${palabraActual}</b>`;
+    juegoModal.classList.add("show");
+  };
 
 // creando botones dle teclado y añadiendo even listeners
 for (let i=97; i<=122; i++) {
